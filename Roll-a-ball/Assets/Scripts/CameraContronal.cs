c20685d3e4efd052637b07a0e46591884be5926f
snowluliang <18651528973@163.com>
2017-03-16 19:54:44 +0800

    add dic

---
+++ Roll-a-ball/Assets/Scripts/CameraContronal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContronal : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offest;
+	// Use this for initialization
+	void Start () {
+        offest = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offest;
+	}
+}
---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+	
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 moveevent = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(moveevent*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+
+ 
+    }
+
+}
